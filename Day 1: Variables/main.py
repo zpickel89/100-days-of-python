be5d15e8c22e02a222e16ee2b743cff1796a2d0e
Zachary Pickel <zpickel@gmail.com>
2024-06-16 13:15:02 -0500

    day one complete

---
+++ Day 1: Variables/main.py
@@ -0,0 +1,6 @@
+""" This is day 1 hello world
+"""
+print("Welcome to the Band Name Generator.")
+city:str = input("What's the name of the city you grew up in?\n")
+pet:str = input("What's the name of a pet?\n")
+print(city + " " + pet)
