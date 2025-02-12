4202c9ae1f515f320bc781f36a102805821a57b5
Zachary Pickel <zpickel@gmail.com>
2025-02-12 08:24:16 -0600

    Day 2 complete

---
+++ Day 2/bmi_calculator.py
@@ -0,0 +1,12 @@
+"""
+BMI calculator exercise 
+"""
+
+height = 1.65
+weight = 84
+
+# Write your code here.
+# Calculate the bmi using weight and height.
+bmi = weight / height ** 2
+
+print(round(bmi, 2))
